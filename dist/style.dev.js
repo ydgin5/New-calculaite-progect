879f0f3d6da0f0c0d0085b0b7c6fd58e3f9cb5b8
ydgin5 <ydginnuch5@gmail.com>
2024-02-13 12:58:11 +0300

    new commit

---
+++ dist/style.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// first number;
+var a = ""; // secont number;
+
+var b = ""; // знак операции;
+
+var sign = "";
+var finish = false; // массив программы(числа);
+
+var digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']; // массив операций;
+
+var action = ['=', '+', 'x', '/']; // экран;
+
+var out = document.querySelector('._0'); // функция очистки переменных;
+
+f;
